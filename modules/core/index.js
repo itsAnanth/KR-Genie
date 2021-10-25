a602046a3eec1e02333055ffc52a3417c3ade078
EJBEAN2op <danonymousarmy@gmail.com>
2021-10-25 22:50:09 +0530

    fix : path

--- modules/core/index.js
+++ modules/core/index.js
@@ -1,4 +1,4 @@
 import eventsHandler from './eventsHandler.js';
-import commandsHandler from './commandHandler';
+import commandsHandler from './commandHandler.js';
 
 export { eventsHandler as handleEvents, commandsHandler as handleCommands };
